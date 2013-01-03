88f43528858ff5389a440ae37228b4da5a1aa7cd
Sinbei <sinbei151@gmail.com>
2013-01-02 19:32:54 -0800

    Create reportstatus.rb

---
+++ reportstatus.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'net/smtp'
+require 'tlsmail'
+require 'mail'
+
+mail = Mail.new
+
+
+mesText = 'This is Status Notification mail from Raspberry Pi.'
+
+# Get Raspbarry Pis' status
+temp = 0.00
+freq = 0.00
+open("/sys/class/thermal/thermal_zone0/temp"){|file|
+        temp = file.read.to_f
+}
+open("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"){|file|
+        freq = file.read.to_f
+}
+temp = temp / 1000
+freq = freq / 1000
+
+mesText += "\nCPU Temperature\t: " + temp.to_s + " Degree Celsius\n"
+mesText +=   "CPU Frequency\t: " + freq.to_s + " MHz\n"
+mesText += "\n\n"
+mesText += "Sended by reportstatus.rb in Raspberry Pi\n"
+mesText += "Sinbei Yoshizaki"
+
+mail[:from] = 'ADDRESS_FROM'
+mail[:to]   = 'ADDRESS_TO'
+mail.subject = 'Raspberry Pi Status'
+mail.body = mesText
+
+smtpserver = Net::SMTP.new('smtp.gmail.com',587)
+smtpserver.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+smtpserver.start('gmail.com','YOUR_GMAIL_ACCOUNT','YOUR_GMAIL_ID', :login){|smtp|
+        smtp.send_message(mail.encoded, mail.from, mail.to)
+}
+
